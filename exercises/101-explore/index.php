453432f7b530a8fbdea353bd88be5f2d858c9f81
Lyza Gardner <lyza@cloudfour.com>
2012-08-14 16:44:58 -0700

    Basic starting exercise

---
+++ exercises/101-explore/index.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Shakespeare: The Tempest | Act III, Scene II</title>
+</head>
+<body>
+<?php include("../content.html"); ?>
+
+</body>
+</html>
